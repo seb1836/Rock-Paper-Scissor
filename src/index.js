26c68e4d52486ebd645ea9d9954bb275d4529ebc
seb1836 <seb111.penna@gmail.com>
2019-04-29 17:17:32 +0200

    finishing function checkWinner

--- src/index.js
+++ src/index.js
@@ -14,7 +14,12 @@ let count = 0;
 const playerSign = [];
 
 class App extends Component {
-  state = { player1Sign: "", player2Sign: "" };
+  state = {
+    player1Sign: "",
+    player2Sign: "",
+    isplayer1winner: false,
+    isplayer2winner: false
+  };
 
   cbPlayer1Sign = () => {
     console.log(this.state.player1Sign, "fdff");
@@ -37,8 +42,39 @@ class App extends Component {
     count++;
   };
 
+  checkWinner(player1Sign, player2Sign) {
+    winningMatches.map(function(winningSign) {
+      if (
+        (player1Sign === winningSign[0] || player2Sign === winningSign[0]) &&
+        (player1Sign === winningSign[1] || player2Sign === winningSign[1])
+      ) {
+        return player1Sign === winningSign[0]
+          ? this.setState({ isplayer1winner: true })
+          : this.setState({ isplayer2winner: true });
+      }
+    });
+  }
+
+  displayWinner() {
+    return this.state.isplayer1winner
+      ? "player1 is winner"
+      : "player2 is winner";
+  }
   renderButtonStartMatch() {
-    return count === 2 ? <button>startMatch</button> : null;
+    return count === 2 &&
+      !this.state.player1winner &&
+      !this.state.player2winner ? (
+      <button
+        onClick={this.checkWinner(
+          this.state.player1Sign,
+          this.state.player2Sign
+        )}
+      >
+        startMatch
+      </button>
+    ) : (
+      this.displayWinner()
+    );
   }
 
   render() {
